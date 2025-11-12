0b6180c62b5a6143405760cd820084235d65d7bc
Oriol Agost Batalla <oriolagobat@gmail.com>
2025-11-12 09:55:53 +0100

    feat: get ready for mas apps, it will work once the pertinent issue is fixed

--- homebrew/default.nix
+++ homebrew/default.nix
@@ -1,13 +1,22 @@
 {userCasks ? [], ... }: {
-    homebrew = {
-	enable = true;
+  homebrew = {
+		enable = true;
 
-	onActivation = {
-	    autoUpdate = true;
-	    upgrade = true;
-	    cleanup = "zap";
-	};
+		onActivation = {
+				autoUpdate = true;
+				upgrade = true;
+				cleanup = "zap";
+		};
 
-	casks = import ./casks.nix ++ userCasks;
-    };
+
+		# This will be the only CLI tool installed via Homebrew
+		# This is because mas is required to install Mac App Store apps
+		# and it has to be installed via Homebrew
+		brews = ["mas"];
+
+		casks = import ./casks.nix ++ userCasks;
+
+		# Disabled bc of https://github.com/mas-cli/mas/issues/1029
+		# masApps = import ./mas.nix;
+  };
 }
---
+++ homebrew/mas.nix
@@ -0,0 +1,4 @@
+{
+  "Amphetamine" = 937984704;
+  "SimpleLogin for Safari" = 6475835429;
+}
