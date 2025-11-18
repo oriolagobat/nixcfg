b424ed4596b8c3be18f67683e624f57de6e9caac
Oriol Agost Batalla <oriolagobat@gmail.com>
2025-11-18 09:56:22 +0100

    feat: update and add temporary overlay for fish build checks on direnv

--- darwin/nixpkgs.nix
+++ darwin/nixpkgs.nix
@@ -1,3 +1,13 @@
 _: {
-    nixpkgs.config.allowUnfree = true;
+    nixpkgs = {
+        config.allowUnfree = true;
+        overlays = [
+            # FIXME: Remove temporary overlay when https://github.com/NixOS/nixpkgs/issues/461406 is released
+            (final: prev: {
+                fish = prev.fish.overrideAttrs (old: {
+                    doCheck = false;
+                });
+            })
+        ];
+    };
 }
