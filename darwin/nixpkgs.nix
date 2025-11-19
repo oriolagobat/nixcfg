feccd2753aeeab3ff4702e9b13da2644f4805972
Oriol Agost Batalla <oriolagobat@gmail.com>
2025-11-19 09:05:58 +0100

    feat: update and remove now uneeded overlay

--- darwin/nixpkgs.nix
+++ darwin/nixpkgs.nix
@@ -1,13 +1,5 @@
 _: {
     nixpkgs = {
         config.allowUnfree = true;
-        overlays = [
-            # FIXME: Remove temporary overlay when https://github.com/NixOS/nixpkgs/issues/461406 is released
-            (final: prev: {
-                fish = prev.fish.overrideAttrs (old: {
-                    doCheck = false;
-                });
-            })
-        ];
     };
 }
