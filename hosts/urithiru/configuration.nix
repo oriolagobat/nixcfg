022c4532deb83f7b331694296340e475cb3a3e15
Oriol Agost Batalla <oriolagobat@gmail.com>
2026-08-30 14:44:11 +0200

    feat: add data mount

--- hosts/urithiru/configuration.nix
+++ hosts/urithiru/configuration.nix
@@ -4,6 +4,7 @@
   imports = [
     ./hardware-configuration.nix
     ../../modules/nixos/boot/urithiru-boot.nix
+    ../../modules/nixos/filesystems/urithiru-filesystems.nix
 
     ../../modules/nixos/base.nix
     ../../modules/nixos/ssh.nix
---
+++ modules/nixos/filesystems/urithiru-filesystems.nix
@@ -0,0 +1,7 @@
+{...}:
+{
+  fileSystems."/data" = {
+    device = "/dev/disk/by-uuid/690ab575-c97b-4ee6-b68f-b04840b61c5b";
+    fsType = "ext4";
+  };
+}
