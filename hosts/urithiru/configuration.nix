355123c9eed9dc0444d588d46df52153c67ce810
Oriol Agost Batalla <oriolagobat@gmail.com>
2026-08-30 11:08:06 +0200

    feat: add urithiru boot

--- hosts/urithiru/configuration.nix
+++ hosts/urithiru/configuration.nix
@@ -3,12 +3,15 @@
 {
   imports = [
     ./hardware-configuration.nix
+    ../../modules/nixos/boot/urithiru-boot.nix
 
     ../../modules/nixos/base.nix
     ../../modules/nixos/ssh.nix
   ];
 
   networking.hostName = "urithiru";
+
+
 
   system.stateVersion = "26.05";
 }
