cdb80f86c5fb43ea6b4e7a6b4a1525bdd9c28efb
Oriol Agost Batalla <oriolagobat@gmail.com>
2026-08-30 12:33:22 +0200

    feat: add secrets to urithiru

---
+++ modules/nixos/secrets.nix
@@ -0,0 +1,19 @@
+{ sops-nix, user, hostName, ... }:
+
+{
+  imports = [
+    sops-nix.nixosModules.sops
+  ];
+
+  sops = {
+    defaultSopsFile = ../../secrets/${hostName}.yaml;
+
+    age.sshKeyPaths = [
+      "/home/${user}/.ssh/${hostName}-secrets"
+    ];
+
+    secrets.userPwd = {
+      neededForUsers = true;
+    };
+  };
+}
---
+++ modules/nixos/users.nix
@@ -0,0 +1,24 @@
+{ config, user, ... }:
+
+{
+  users = {
+    mutableUsers = false;
+
+    users."${user}" = {
+      isNormalUser = true;
+
+      hashedPasswordFile =
+        config.sops.secrets.userPwd.path;
+
+      extraGroups = [
+        "wheel"
+      ];
+
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFvDT+VInpmTuxYjLXzyE+3Mn3IjI4asm0aS7O3SDH97 oriolagobat@lift"
+      ];
+    };
+  };
+
+  security.sudo.enable = true;
+}
