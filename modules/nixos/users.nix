e1215323b220b7efff76d0a8bc07ff3cf11ab5c1
Oriol Agost Batalla <oriolagobat@gmail.com>
2026-08-30 13:17:28 +0200

    feat: fix public key

--- modules/nixos/users.nix
+++ modules/nixos/users.nix
@@ -15,7 +15,7 @@
       ];
 
       openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFvDT+VInpmTuxYjLXzyE+3Mn3IjI4asm0aS7O3SDH97 oriolagobat@lift"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMq9Ayue9ebiyLAomBGhH5I7bZ8u+w8VAihVFmF4juJM oriolagobat@lift"
       ];
     };
   };
